6a517adf2d43ace3e845de5a370f90a98c0d536f
pyama <pyama@pepabo.com>
2022-12-28 19:54:57 +0900

    やっぱgsug

--- app.rb
+++ app.rb
@@ -51,6 +51,7 @@ class WebApp < Sinatra::Base
 <html>
     EOS
     email = client.get_user_profile('me').email_address
+    c.gsub!(/%email%/, email)
     ERB.new(c, trim_mode: '-').result(binding)
   end
   # rubocop:enable all
